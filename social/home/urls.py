2fbf71f2e4395c6c709e0de4216c5977c874b27c
Elnaz Babakhani <elnaz.babakhani71@gmail.com>
2023-08-30 14:48:30 -0700

    update comments

--- social/home/urls.py
+++ social/home/urls.py
@@ -5,4 +5,6 @@ app_name='home'
 urlpatterns=[
     path('',views.HomeView.as_view(),name='home'),
     path('detail/<int:id>/<slug:slug>',views.PostDetailView.as_view(), name='detail'),
+    path('post/delete/<int:id>', views.PostDeleteView.as_view(), name='post_delete'),
+    path('post/update/<int:id>', views.PostUpdateView.as_view(), name='post_update'),
 ]
--- social/home/views.py
+++ social/home/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
+from typing import Any
+from django import http
+from django.http.response import HttpResponse
+from django.shortcuts import render, redirect
 from django.views import View
 from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
+from .forms import PostUpdateForm
+from django.utils.text import slugify
 
 
 class HomeView(View):
@@ -12,3 +19,46 @@ class PostDetailView(View):
     def get(self, request,id,slug):
         post=Post.objects.get(id=id, slug=slug)
         return render(request, 'home/detail.html',{'post':post})
+    
+
+class PostDeleteView(LoginRequiredMixin,View):
+    def get(self, request, id):
+        post=Post.objects.get(id=id)
+        if post.user.id==request.user.id:
+            post.delete()
+            messages.success(request,'Post deleted successfully', 'success')
+            return redirect('home:home')
+        messages.error(request,'You have no permission', 'danger')
+        return redirect('home:home')
+
+class PostUpdateView(LoginRequiredMixin, View):
+    form_class=PostUpdateForm
+
+    def setup(self, request, *args: Any, **kwargs: Any):
+        self.post_instance=Post.objects.get(id=kwargs['id'])
+        return super().setup(request, *args, **kwargs)
+
+    def dispatch(self, request, *args: Any, **kwargs: Any):
+        post=self.post_instance
+        if not post.user.id==request.user.id:
+            messages.error(request,'there is problem with data','danger')
+            return redirect('home:detail', post.id, post.slug)
+        return super().dispatch(request, *args, **kwargs)
+    
+
+    def get(self, request, id):
+        post=self.post_instance
+        form=self.form_class(instance=post)
+        return render(request, 'home/post_update.html', {'form':form})
+    
+    def post(self, request,id):
+        post=self.post_instance
+        form=self.form_class(request.POST, instance=post)
+        if form.is_valid():
+            new_post=form.save(commit=False)
+            new_post.slug=slugify(form.cleaned_data['body'][:30])
+            new_post.save()
+            messages.success(request, 'Post updated successfully','success')
+            return redirect('home:home')
+        messages.error(request,'invalid data','danger')
+        return redirect('home:detail', post.id, post.slug)
